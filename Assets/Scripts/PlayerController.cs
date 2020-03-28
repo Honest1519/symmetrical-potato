a10583a90553fe992c5d896c56847f0c2c5578dd
QuinnSuligoy <56174399+QuinnSuligoy@users.noreply.github.com>
2020-03-27 17:54:18 -0700

    Added Wall Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,31 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float horzInput;
     public float jumpForce;
+    public float wallspeed;
+    public float wallJumpForce;
+
+    public bool isWallSliding = false;
+
+    public string facing = "Right";
 
     private Vector2 velocity;
+    public Vector2 wallJumpDirection;
 
     private Rigidbody2D playerRB;
 
     private GroundCheck groundCheck;
+    private WallDetector wallCheck;
     // Start is called before the first frame update
     void Start()
     {
         //Get the components
         playerRB = GetComponent<Rigidbody2D>(); 
         groundCheck = GameObject.Find("GroundCheck").GetComponent<GroundCheck>();
+        wallCheck = GameObject.Find("WallDetector").GetComponent<WallDetector>();
+
+        wallJumpDirection.Normalize();
+
+
 
     }
 
@@ -33,27 +46,56 @@ public class PlayerController : MonoBehaviour
         velocity = playerRB.velocity;
         velocity.x = pos.x;
         playerRB.velocity = velocity;
-
-        
     }
 
     void Update()
     {
-        //Jump
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (groundCheck.grounded)
-            {
-                Jump();
-            }
-        }
+        
      
         //Get Inputs
         horzInput = Input.GetAxis("Horizontal");
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        //Face Left or Right
+        if(horzInput < 0 )
+        {
+            facing = "Left";
+        }
+        if(horzInput > 0 )
+        {
+            facing = "Right";
+        }
+
+        //WallSlide
+        if(wallCheck.Walled && playerRB.velocity.y < 0)
+        {
+            if(playerRB.velocity.y < -wallspeed)
+            {
+                isWallSliding = true;
+                playerRB.velocity = new Vector2(playerRB.velocity.x, -wallspeed);
+            }
+            isWallSliding = false;
+        }
+        
+
     }
 
     void Jump()
     {
-        playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if(groundCheck.grounded == true)
+        {
+            playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        else if((isWallSliding || wallCheck.Walled) && horzInput != 0)
+        {
+            Vector2 forceToAdd = new Vector2(wallJumpForce * wallJumpDirection.x * horzInput, wallJumpForce * wallJumpDirection.y);
+            playerRB.velocity = new Vector2(playerRB.velocity.x, 0);
+            playerRB.AddForce(forceToAdd, ForceMode2D.Impulse);
+        }
     }
+
+    
 }
---
+++ Assets/Scripts/WallDetector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetector : MonoBehaviour
+{
+    public bool Walled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Ground"))
+        {
+            Walled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Ground"))
+        {
+            Walled = false;
+        }
+    }
+}
