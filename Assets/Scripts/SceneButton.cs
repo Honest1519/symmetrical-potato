1cb195287682e66343d439f25b931b848c0a2b50
QuinnSuligoy <56174399+QuinnSuligoy@users.noreply.github.com>
2020-04-01 19:44:09 -0700

    ChangeScenes

---
+++ Assets/Scripts/SceneButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            PlayGame();
+        }
+    }
+
+
+}
